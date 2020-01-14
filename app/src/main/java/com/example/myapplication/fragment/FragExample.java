061c9e8329ded308ad1ed33edcf93c3fe1e500ce
sumin <ekrxjwh2009@kaist.ac.kr>
2020-01-14 21:51:50 +0900

    dfsdf

--- app/src/main/java/com/example/myapplication/fragment/FragExample.java
+++ app/src/main/java/com/example/myapplication/fragment/FragExample.java
@@ -1,11 +1,17 @@
 package com.example.myapplication.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,25 +19,99 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FragExample extends Fragment {
 
     private Context mContext;
-//    SharedPreferences pref = requireContext().getSharedPreferences("pref",Context.MODE_PRIVATE);
-////    String accountName = pref.getString("key1", "hi");
+    SharedPreferences pref;
+    String accountName;
 
     TextView textView1;
 
+    private EditText user_chat, user_edit;
+    private Button user_next;
+    private ListView chat_list;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();//데이터 베이스에서 정의된걸로
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();//데이터 베이스에서 정의된걸로
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SharedPreferences pref = requireContext().getSharedPreferences("pref", Context.MODE_PRIVATE);
+        accountName = pref.getString("key1", "");
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.activity_frag_ex1, null);
-        textView1 = rootView.findViewById(R.id.textView);
-//        textView1.setText(accountName + "1");
+        View rootView = inflater.inflate(R.layout.frag_chat, null);
+
+        user_chat = (EditText) rootView.findViewById(R.id.user_chat);//등록한 이메일로 사용자 표시
+        user_edit = (EditText) rootView.findViewById(R.id.user_edit); //문자 보내는 부분
+        user_next = (Button) rootView.findViewById(R.id. user_next);//전송 버튼
+        chat_list = (ListView)rootView.findViewById(R.id.chat_list);//채팅방 목록
+
+        user_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user_edit.getText().toString().equals("") || user_chat.getText().toString().equals(""))
+                    return;
+
+                Intent intent = new Intent(getActivity(), ChatActivity.class);
+                intent.putExtra("chatName", user_chat.getText().toString());
+                intent.putExtra("userName", user_edit.getText().toString());
+                startActivity(intent);
+            }
+        });
+        showChatList();
 
         return rootView;
+    }
+
+
+    private void showChatList() {
+        // 리스트 어댑터 생성 및 세팅
+        final ArrayAdapter<String> adapter
+
+                = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1);
+        chat_list.setAdapter(adapter);
+
+        // 데이터 받아오기 및 어댑터 데이터 추가 및 삭제 등..리스너 관리
+        databaseReference.child("chat").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Log.e("LOG", "dataSnapshot.getKey() : " + dataSnapshot.getKey());
+                adapter.add(dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
